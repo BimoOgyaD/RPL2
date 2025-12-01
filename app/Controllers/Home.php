7f8a0eeae225965177fe6047872bfe9f8aeafb35
BimoOgyaD <156457176+BimoOgyaD@users.noreply.github.com>
2025-12-02 00:02:28 +0700

    Delete app directory

--- app/Controllers/Home.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Controllers;
-class Home {
-    public function index() {
-        $title = 'MooHub - Biodata Perusahaan';
-        include __DIR__ . '/../Views/layout.php';
-    }
-}
